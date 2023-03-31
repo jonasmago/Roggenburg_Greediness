6cb7646737bbec0ca39820f6a23ee631eb019215
Jonas Mago <jonas.h.mago@gmail.com>
2023-03-31 15:33:17 +0200

    going back to previsou version

--- axelrod_original/axelrod_multiple runs.py
+++ axelrod_original/axelrod_multiple runs.py
@@ -1,5 +1,6 @@
 """
 Implementation of the Axelrod model of cultural convergence and polarisation
+
 AXELROD R (1997) The dissemination of culture - A model with local convergence and global polarization. 
     Journal of Conflict Resolution 41(2), pp. 203-226.]
 """
@@ -12,18 +13,25 @@ from matplotlib import pyplot
 from numpy.random import random
 from matplotlib import cm
 import random as rd
+from itertools import islice
+def take(n, iterable):
+    """Return the first n items of the iterable as a list."""
+    return list(islice(iterable, n))
 
 # Define constants
 SIZE = 7 # grid of agents with SIZE * SIZE
-RUNS = 100000 # number of iterations
+RUNS = 100 # number of iterations
 PROB_SHIFT = 0.1 # additional winning probability for the agent with more money
 MONEY_GAMBLE_SHARE = 0.004 # play in a game for this share of total money in the economy
+FIXED_SHARE = 1 # 1: agents play for a fixed amount of money, 0: play for share of cake via MONEY_GAMBLE_SHARE
+MONEY_GAMBLE_FIXED = 0.01 # fixed money gamble parameter
 START_WEALTH_DISTRIBUTION = 4 # 1: everyone gets 2/7, 2: random draw from uniform(0,4/7), 3: random draw from beta(2,5): replicates realistic wealth distribution between 0 and 1 with expected value 2/7; 4: everyone gets 1
 GREEDINESS_DISTRIBUTION = 4 # 1: everyone gets 0.5, 2: random draw from uniform(0,1), 3: random draw from beta(5,5): some agents are very greedy and some not at all; 4: half is 0.6, other half is 0.4
 TAX_RATE = 0.00003 # flat tax on wealth, redistributed lump-sum every iteration. Set to 0 to get model without state
 NO_DEBT = 1 # 1: switch off that agents can have money smaller or equal to zero, 0: allow debt
 VISUAL = 0 # 1: shows plots while calculating, 0: does not show plots
 # Seed pseudo-random number generator
+N_SIM = 10 # number of model simulations
 
 #rd.seed(1)
 
@@ -67,13 +75,13 @@ class Agent():
             if rd.uniform(0, 1) < win_probability:    
                 model.agents[target].money = model.agents[target].money - MONEY_GAMBLE
                 self.money = self.money + MONEY_GAMBLE
-                model.agent[target].performance.append(0)
+                model.agents[target].performance.append(0)
                 self.performance.append(1)
                 
             else: 
                 model.agents[target].money = model.agents[target].money + MONEY_GAMBLE
                 self.money = self.money - MONEY_GAMBLE
-                model.agent[target].performance.append(1)
+                model.agents[target].performance.append(1)
                 self.performance.append(0)
 
 class Axelrod():
@@ -83,9 +91,12 @@ class Axelrod():
         self.agents = [Agent() for i in range(SIZE**2)]
         self.n_agents = SIZE**2
         
-        # init result vector 
+        # init result vector
         self.results = {} # empty dictionary
         self.results = {i: [] for i in range(SIZE**2)}
+        self.results['greediness'] = [] # empty dictionary
+        self.results['performance'] = {} # empty dictionary        
+        self.results['performance'] = {i: [] for i in range(SIZE**2)}
         # add results for each participant to their corresponding key
         for i in range(self.n_agents):
             self.results[i] = []
@@ -93,17 +104,21 @@ class Axelrod():
     
     def tick(self):
         "Runs a single cycle of the simulation"
-        try:
-            active = rd.choice(list(enumerate(self.agents)))
-            passive = rd.choice(list(enumerate(self.agents)))
-            while active[0] == passive[0]:
-                passive = rd.choice(list(enumerate(self.agents)))     
-            active[1].game(MONEY_GAMBLE_SHARE * sum([self.agents[i].money for i in range(SIZE**2)]), passive[0], self)
-            total_wealth = sum([self.agents[i].money for i in range(0, SIZE**2)])
-            for i in range(0, SIZE**2):
-                self.agents[i].money = (1-TAX_RATE) * self.agents[i].money + (TAX_RATE * total_wealth / (SIZE**2))
-        except IndexError:
-            self.tick()
+        for i in range(0, SIZE**2):    
+            try:
+                active = list(enumerate(self.agents))[i]
+                passive = rd.choice(list(enumerate(self.agents)))
+                while active[0] == passive[0]:
+                    passive = rd.choice(list(enumerate(self.agents))) 
+                if FIXED_SHARE == 1:
+                    active[1].game(MONEY_GAMBLE_FIXED, passive[0], self)
+                elif FIXED_SHARE == 0:
+                    active[1].game(MONEY_GAMBLE_SHARE * sum([self.agents[i].money for i in range(SIZE**2)]), passive[0], self)
+                total_wealth = sum([self.agents[i].money for i in range(0, SIZE**2)])
+                for i in range(0, SIZE**2):
+                   self.agents[i].money = (1-TAX_RATE) * self.agents[i].money + (TAX_RATE * total_wealth / (SIZE**2))
+            except IndexError:
+               self.tick()
  
     def show_state(self):
         for n in range(0, SIZE):
@@ -120,6 +135,8 @@ class Axelrod():
         print("Initial state of the model:")
         print()
         self.show_state()
+        for i in range (SIZE**2):
+            self.results['greediness'].append(self.agents[i].greediness)
         print()
         print("Running simulation...")
         for r in range(1,RUNS):
@@ -132,6 +149,9 @@ class Axelrod():
             self.tick()
         print("Final state of the model:")
         self.show_state()
+        for i in range (SIZE**2):
+            self.results[i].append(self.agents[i].performance)
+            self.results['performance'][i].append(self.agents[i].performance)
 
     def visualize_output(self):
         color = []
@@ -142,5 +162,11 @@ class Axelrod():
         pyplot.show()
 
 if __name__ == "__main__":
-    model = Axelrod()
-    model.run_sim()
+    # initialize results from different simulations
+    results_sim = {} # empty dictionary
+    results_sim = {i: [] for i in range(1, N_SIM)}
+    for s in range(1, N_SIM):
+        model = Axelrod()
+        model.run_sim()
+        results_sim[s].append(model.results)
+    print(take(2, results_sim[1]))
